52ea240a1503a8607086544850f2590a5b3d69ef
Chrisctr <credfern96@gmail.com>
2020-04-09 13:59:24 +0100

    Changed from main to ims package name

--- src/main/java/com/qa/ims/DB.java
+++ src/main/java/com/qa/ims/DB.java
@@ -1,4 +1,4 @@
-package com.qa.main;
+package com.qa.ims;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/main/java/com/qa/ims/DBConfiguration.java
+++ src/main/java/com/qa/ims/DBConfiguration.java
@@ -1,4 +1,4 @@
-package com.qa.main;
+package com.qa.ims;
 
 public class DBConfiguration {
 
