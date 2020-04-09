5e0398d3afa78d1e8de9d83cf40cb3ad343892c5
Chrisctr <credfern96@gmail.com>
2020-04-09 13:08:21 +0100

    Initiated jar with dependancies

--- src/main/java/com/qa/main/Runner.java
+++ src/main/java/com/qa/main/Runner.java
@@ -2,7 +2,11 @@ package com.qa.main;
 
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
 public class Runner {
+
+	public static final Logger LOGGER = Logger.getLogger(Runner.class);
 
 	public static void main(String[] args) throws SQLException {
 		DB db = new DB();
