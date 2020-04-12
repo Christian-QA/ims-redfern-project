35c20572c15c48adb8a9499c6f6aae034c5760fd
Chrisctr <credfern96@gmail.com>
2020-04-12 17:47:04 +0100

    Deleted old crud system

--- src/main/java/com/qa/ims/Runner.java
+++ src/main/java/com/qa/ims/Runner.java
@@ -10,9 +10,6 @@ public class Runner {
 
 	public static void main(String[] args) throws SQLException {
 		DB db = new DB();
-		/// db.createCustomer("John", "Augustus", "Harper", 25, "example@gmail.com",
-		/// "24 New Spring Lane, " + "London, NW2 1HE", "TheBartManxxx123");
-		db.readCustomers();
-		/// db.deleteCustomer(0);
+
 	}
 }
