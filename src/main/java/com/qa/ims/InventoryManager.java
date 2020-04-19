171642572058877200183a3d09adb3b8b663a637
Chrisctr <credfern96@gmail.com>
2020-04-19 19:42:26 +0100

    Moved getTableSelectCommand and getDMLCommands to after the LOGGER to show custom messages on one line, leaving descriptions to the help option

--- src/main/java/com/qa/ims/InventoryManager.java
+++ src/main/java/com/qa/ims/InventoryManager.java
@@ -21,14 +21,14 @@ public class InventoryManager {
 		LOGGER.info("Please insert password: ");
 		String password = Utils.getInput();
 
-		TableSelectCommand tableSelectCommand = TableSelectCommand.getTableSelectCommand();
 		LOGGER.info("Hello, " + username + " how can I help you today?");
 		System.out.println(
 				"1): Review Customers      2): Review Products      3): Review Orders      4): Help      5): Stop");
+		TableSelectCommand tableSelectCommand = TableSelectCommand.getTableSelectCommand();
 
-		DMLCommands DMLCommand = DMLCommands.getDMLCommands();
 		LOGGER.info("You have selected '" + tableSelectCommand + "'. How would you like to proceed?");
 		System.out.println("1): Create      2): Read      3): Update      4): Delete      5): Help      6): Back");
+		DMLCommands DMLCommand = DMLCommands.getDMLCommands();
 
 		switch (tableSelectCommand) {
 		case CUSTOMER:
