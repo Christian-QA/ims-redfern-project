3b294c8efd4cfee39374e3284d1a339ca4eccf7b
Chrisctr <credfern96@gmail.com>
2020-04-19 22:56:44 +0100

    Added C, R and D functionality to customer table. U functionality still in progress. Need end statement of D for when task is complete

--- src/main/java/com/qa/ims/persistence/dataaccessobjects/CustomerDataAccessObject.java
+++ src/main/java/com/qa/ims/persistence/dataaccessobjects/CustomerDataAccessObject.java
@@ -33,12 +33,17 @@ public class CustomerDataAccessObject implements DataAccessObject<CustomerProfil
 	}
 
 	CustomerProfile customerProfileSet(ResultSet resultSet) throws SQLException {
-		Long id = resultSet.getLong("id");
+		Long id = resultSet.getLong("customer_id");
 		String forename = resultSet.getString("forename");
 		String surname = resultSet.getString("surname");
 		return new CustomerProfile(id, forename, surname);
 	}
 
+	/**
+	 * Reads all customers from the database
+	 * 
+	 * @return A list of customers
+	 */
 	@Override
 	public List<CustomerProfile> readAll() {
 		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
@@ -56,22 +61,86 @@ public class CustomerDataAccessObject implements DataAccessObject<CustomerProfil
 		return new ArrayList<>();
 	}
 
-	@Override
-	public CustomerProfile create(CustomerProfile t) {
-		// TODO Auto-generated method stub
+	public CustomerProfile readLatest() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement
+						.executeQuery("SELECT * FROM customers ORDER BY customer_id DESC LIMIT 1");) {
+			resultSet.next();
+			return customerProfileSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
 		return null;
 	}
 
+	/**
+	 * Creates a customer in the database
+	 * 
+	 * @param customer - takes in a customer object. id will be ignored
+	 */
 	@Override
-	public CustomerProfile update(CustomerProfile t) {
-		// TODO Auto-generated method stub
+	public CustomerProfile create(CustomerProfile customer) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("insert into customers(forename, surname) values('" + customer.getForename() + "','"
+					+ customer.getSurname() + "')");
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
 		return null;
 	}
 
+	public CustomerProfile readCustomer(Long id) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT FROM customers where customer_id = " + id);) {
+			resultSet.next();
+			return customerProfileSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Updates a customer in the database
+	 * 
+	 * @param customer - takes in a customer object, the id field will be used to
+	 *                 update that customer in the database
+	 * @return
+	 */
+	@Override
+	public CustomerProfile update(CustomerProfile customer) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("update customers set forename ='" + customer.getForename() + "', surname ='"
+					+ customer.getSurname() + "' where customer_id =" + customer.getId());
+			return readCustomer(customer.getId());
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Deletes a customer in the database
+	 * 
+	 * @param id - id of the customer
+	 */
 	@Override
 	public void delete(long id) {
-		// TODO Auto-generated method stub
-
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("delete from customers where customer_id = " + id);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
 	}
-
 }
