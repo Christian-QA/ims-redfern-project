2a61f935916f4f6f981f46065d3e74a7a25ea30c
Chrisctr <credfern96@gmail.com>
2020-04-19 13:42:54 +0100

    fixed issues with runner

--- src/main/java/com/qa/ims/Runner.java
+++ src/main/java/com/qa/ims/Runner.java
@@ -8,11 +8,23 @@ public class Runner {
 
 	public static final Logger LOGGER = Logger.getLogger(Runner.class);
 
+<<<<<<< Updated upstream
 	public static void main(String[] args) throws SQLException {
 		DB db = new DB();
 		/// db.createCustomer("John", "Augustus", "Harper", 25, "example@gmail.com",
 		/// "24 New Spring Lane, " + "London, NW2 1HE", "TheBartManxxx123");
 		db.readCustomers();
 		/// db.deleteCustomer(0);
+=======
+	public static void main(String[] args) {
+
+		BasicConfigurator.configure();
+
+		InventoryManager ims = new InventoryManager();
+		ims.initiateSystem();
+
+		/// DB db = new DB();
+
+>>>>>>> Stashed changes
 	}
 }
